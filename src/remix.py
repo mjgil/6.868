bd0842bbad416b5074b6bd189f738dee12099c6d
Malcom Gilbert <malcomgilbert@gmail.com>
2013-05-15 18:22:10 -0400

    fixed sound shifting

--- src/remix.py
+++ src/remix.py
@@ -9,35 +9,57 @@ output_filename = "../audio/processed/fifth_test3"
 # change tempo
 
 # change pitch
-def shift_each_bar():
+def shift_each_bar_semitones():
   soundtouch = modify.Modify()
   audiofile = audio.LocalAudioFile(input_filename)
-  print len(audiofile.data)
   out_shape = (len(audiofile.data),)
   out_data = audio.AudioData(shape=out_shape, numChannels=1, sampleRate=44100)
   bars = audiofile.analysis.bars
   for i, bar in enumerate(bars):
-    print bar.children()
-    for j, beat in enumerate(bar.children()):
-      print i
-      print beat
-      new_beat = soundtouch.shiftPitch(audiofile[beat], (i*2))
+    if (len(bar.children()) > 3):
+      top = bar.children()[0]
+      chord = bar.children()[1]
+      root = bar.children()[2]
+      middle = bar.children()[3]
+
+      new_beat1 = soundtouch.shiftPitchSemiTones(audiofile[root], (i*2))
+      out_data.append(new_beat1)
+
+      new_beat2 = soundtouch.shiftPitchSemiTones(audiofile[middle], (i*2))
+      out_data.append(new_beat2)
+
+      new_beat3 = soundtouch.shiftPitchSemiTones(audiofile[top], (i*2))
+      out_data.append(new_beat3)
+
+      new_beat = audio.mix(new_beat1, new_beat3, 0.5)
+      new_beat = audio.mix(new_beat, new_beat2, 0.66)
       out_data.append(new_beat)
   out_data.encode(output_filename + "EachBarShiftedSemiTones.mp3")
 
-def shift_each_bar():
+def shift_each_bar_arbitrary():
   soundtouch = modify.Modify()
   audiofile = audio.LocalAudioFile(input_filename)
-  print len(audiofile.data)
   out_shape = (len(audiofile.data),)
   out_data = audio.AudioData(shape=out_shape, numChannels=1, sampleRate=44100)
   bars = audiofile.analysis.bars
   for i, bar in enumerate(bars):
-    print bar.children()
-    for j, beat in enumerate(bar.children()):
-      print i
-      print beat
-      new_beat = soundtouch.shiftPitch(audiofile[beat], ((i+1)*2)*0.1)
+    if (len(bar.children()) > 3):
+      top = bar.children()[0]
+      chord = bar.children()[1]
+      root = bar.children()[2]
+      middle = bar.children()[3]
+
+      new_beat1 = soundtouch.shiftPitch(audiofile[root], ((i+1)*2)*0.1)
+      out_data.append(new_beat1)
+
+      new_beat2 = soundtouch.shiftPitch(audiofile[middle], ((i+1)*2)*0.1)
+      out_data.append(new_beat2)
+
+      new_beat3 = soundtouch.shiftPitch(audiofile[top], ((i+1)*2)*0.1)
+      out_data.append(new_beat3)
+
+      new_beat = audio.mix(new_beat1, new_beat3, 0.5)
+      new_beat = audio.mix(new_beat, new_beat2, 0.66)
       out_data.append(new_beat)
   out_data.encode(output_filename + "EachBarShiftedArbitrary.mp3")
 
@@ -86,4 +108,4 @@ def reverse_beats():
 # reverse_beats()
 # extract_first_beat()
 # extract_first_beat_and_shift()
-shift_each_bar()
+shift_each_bar_arbitrary()
