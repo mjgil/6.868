3a75eb1d5a7b868ef8a0a63775c8190f3cb5e7da
Malcom Gilbert <malcomgilbert@gmail.com>
2013-05-17 02:57:49 -0400

    refactor 3 note class

--- src/remix.py
+++ src/remix.py
@@ -40,6 +40,8 @@ class BaseRemix(object):
     print self.output_file
 
   def process(self):
+      self.analyze()
+
       fn = getattr(self, self.remix_type)
       assert hasattr(fn, '__call__'), 'Invalid Remix Type'
       fn()
@@ -56,137 +58,115 @@ class BaseRemix(object):
   def all_notes_pitch_shift(self):
       raise NotImplementedError
 
+  def get_beats(self):
+      raise NotImplementedError
+
   def encode(self, out):
     out.encode(self.output_file)
     prev_path = os.path.join(statics.input_path,self.Name,'previous.wav')
     out.encode(prev_path)
+
+  def analyze(self):
+    self.soundtouch = modify.Modify()
+    self.audiofile = audio.LocalAudioFile(self.input_file)
+    self.bars = audiofile.analysis.bars
+    self.out_shape = (len(audiofile.data),)
+    self.out_data = audio.AudioData(shape=self.out_shape, numChannels=1, sampleRate=44100)
+    self.get_beats()
+
 
 class ThreeNoteRemix(BaseRemix):
   Name = "three_note"
   def __init__(self, args):
     super(ThreeNoteRemix, self).__init__(args)
 
-  def change_tempo(self):
-    audiofile = audio.LocalAudioFile(self.input_file)
-    bars = audiofile.analysis.bars
-    collect = []
-
-    for bar in bars:
+  def get_beats(self):
+    for bar in self.bars:
       if (len(bar.children()) > 3):
-        for x in range(8):
-          top = bar.children()[0]
-          chord = bar.children()[1]
-          root = bar.children()[2]
-          middle = bar.children()[3]
-          beats = [root, middle, top, chord]
+        top = bar.children()[0]
+        chord = bar.children()[1]
+        root = bar.children()[2]
+        middle = bar.children()[3]
+        self.beats = [root, middle, top, chord]
 
-          for i, beat in enumerate(beats):
-            ratio = self.remix_amount + 0.5
-            beat_audio = beat.render()
-            scaled_beat = dirac.timeScale(beat_audio.data, ratio)
-            ts = audio.AudioData(ndarray=scaled_beat, shape=scaled_beat.shape, 
-                            sampleRate=audiofile.sampleRate, numChannels=scaled_beat.shape[1])
-            collect.append(ts)
-        break
+  def change_tempo(self):
+    collect = []
+    for x in range(8):
+      for beat in self.beats:
+        ratio = self.remix_amount + 0.5
+        beat_audio = beat.render()
+        scaled_beat = dirac.timeScale(beat_audio.data, ratio)
+        ts = audio.AudioData(ndarray=scaled_beat, shape=scaled_beat.shape, 
+                        sampleRate=self.audiofile.sampleRate, numChannels=scaled_beat.shape[1])
+        collect.append(ts)
 
     out = audio.assemble(collect, numChannels=2)
     self.encode(out)
 
   def change_note_order(self):
-    audiofile = audio.LocalAudioFile(self.input_file)
-    bars = audiofile.analysis.bars
     collect = []
+    for x in range(8):
+      beats = self.beats[:-1]
+      random.shuffle(beats)
+      chord = self.beats[-1]
+      beats.append(chord)
 
-    print len(bars)
-
-    for bar in bars:
-      if (len(bar.children()) > 3):
-
-        for x in range(8):
-          top = bar.children()[0]
-          chord = bar.children()[1]
-          root = bar.children()[2]
-          middle = bar.children()[3]
-          beats = [root, middle, top]
-          random.shuffle(beats)
-          beats.append(chord)
-
-          for i, beat in enumerate(beats):
-            beat_audio = beat.render()
-            scaled_beat = dirac.timeScale(beat_audio.data, 1.0)
-            ts = audio.AudioData(ndarray=scaled_beat, shape=scaled_beat.shape, 
-                            sampleRate=audiofile.sampleRate, numChannels=scaled_beat.shape[1])
-            collect.append(ts)
-        break
+      for beat in beats:
+        beat_audio = beat.render()
+        scaled_beat = dirac.timeScale(beat_audio.data, 1.0)
+        ts = audio.AudioData(ndarray=scaled_beat, shape=scaled_beat.shape, 
+                        sampleRate=self.audiofile.sampleRate, numChannels=scaled_beat.shape[1])
+        collect.append(ts)
 
     out = audio.assemble(collect, numChannels=2)
     self.encode(out)
 
   def one_note_pitch_shift(self):
-    soundtouch = modify.Modify()
-    audiofile = audio.LocalAudioFile(self.input_file)
-    out_shape = (len(audiofile.data),)
-    out_data = audio.AudioData(shape=out_shape, numChannels=1, sampleRate=44100)
-    bars = audiofile.analysis.bars
     random_index = random.randrange(0,3)
-    for i, bar in enumerate(bars):
-      if (len(bar.children()) > 3):
-        for x in range(8):
-          top = bar.children()[0]
-          chord = bar.children()[1]
-          root = bar.children()[2]
-          middle = bar.children()[3]
+    for x in range(8):
+      beats = self.beats[:-1]
+      beat_list = []
+      for j, beat in enumerate(beats):
+        shift_ratio = 1
+        if j == random_index:
+          shift_ratio = shift_ratio * self.remix_amount
+        new_beat = self.soundtouch.shiftPitch(self.audiofile[beat], shift_ratio)
+        out_data.append(new_beat)
+        beat_list.append(new_beat)
 
-          beats = [root, middle, top]
-          beat_list = []
-          for j, beat in enumerate(beats):
-            shift_ratio = 1
-            if j == random_index:
-              shift_ratio = shift_ratio * self.remix_amount
-            new_beat = soundtouch.shiftPitch(audiofile[beat], shift_ratio)
-            out_data.append(new_beat)
-            beat_list.append(new_beat)
-
-          new_beat = audio.mix(beat_list[0], beat_list[1], 0.5)
-          new_beat = audio.mix(new_beat, beat_list[2], 0.66)
-          out_data.append(new_beat)
-        break
+      new_beat = audio.mix(beat_list[0], beat_list[1], 0.5)
+      new_beat = audio.mix(new_beat, beat_list[2], 0.66)
+      out_data.append(new_beat)
     self.encode(out_data)
 
   def all_notes_pitch_shift(self):
-    soundtouch = modify.Modify()
-    audiofile = audio.LocalAudioFile(self.input_file)
-    out_shape = (len(audiofile.data),)
-    out_data = audio.AudioData(shape=out_shape, numChannels=1, sampleRate=44100)
-    bars = audiofile.analysis.bars
-    for i, bar in enumerate(bars):
-      if (len(bar.children()) > 3):
-        for x in range(8):
-          top = bar.children()[0]
-          chord = bar.children()[1]
-          root = bar.children()[2]
-          middle = bar.children()[3]
+    for x in range(8):
+      beats = self.beats[:-1]
+      beat_list = []
+      for j, beat in enumerate(beats):
+        shift_ratio = self.remix_amount
+        new_beat = self.soundtouch.shiftPitch(self.audiofile[beat], shift_ratio)
+        out_data.append(new_beat)
+        beat_list.append(new_beat)
 
-
-          beats = [root, middle, top]
-          beat_list = []
-          for j, beat in enumerate(beats):
-            shift_ratio = self.remix_amount
-            new_beat = soundtouch.shiftPitch(audiofile[beat], shift_ratio)
-            out_data.append(new_beat)
-            beat_list.append(new_beat)
-
-          new_beat = audio.mix(beat_list[0], beat_list[1], 0.5)
-          new_beat = audio.mix(new_beat, beat_list[2], 0.66)
-          out_data.append(new_beat)
-
-        break
+      new_beat = audio.mix(beat_list[0], beat_list[1], 0.5)
+      new_beat = audio.mix(new_beat, beat_list[2], 0.66)
+      out_data.append(new_beat)
     self.encode(out_data)
 
 class FourNoteRemix(BaseRemix):
   Name = "four_note"
   def __init__(self, args):
     super(FourNoteRemix, self).__init__(args)
+
+  def get_beats(self):
+    for bar in self.bars:
+      top = bar.children()[4]
+      chord = bar.children()[0]
+      root = bar.children()[1]
+      middle = bar.children()[2]
+      middle1 = bar.children()[3]
+      beats = [root, middle, middle1, top, chord]
 
 
   def change_tempo(self):
